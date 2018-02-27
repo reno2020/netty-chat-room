e51d44b0a0030bf55c20feb3bb9a12e0f8e5f949
zhangjc <zhangjinci@ppmoney.com>
2018-02-27 10:47:17 +0800

    s

--- chat-room-server/src/main/java/org/throwable/server/handler/ChatRoomServerInitializer.java
+++ chat-room-server/src/main/java/org/throwable/server/handler/ChatRoomServerInitializer.java
@@ -26,7 +26,5 @@ public class ChatRoomServerInitializer extends ChannelInitializer<SocketChannel>
         pipeline.addLast("decoder", new StringDecoder());
         pipeline.addLast("encoder", new StringEncoder());
         pipeline.addLast("handler", new ChatRoomServerHandler());
-
-        System.out.println();
     }
 }
